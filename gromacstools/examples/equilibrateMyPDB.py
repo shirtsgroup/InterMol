b725bd50d3867be410cb43a720dec58e89562a6f
vvoelz <vvoelz@a9e5ec5c-6028-0410-9666-b03508a98303>
2007-07-09 20:29:44 +0000

    gromacstools/examples now works

--- gromacstools/examples/equilibrateMyPDB.py
+++ gromacstools/examples/equilibrateMyPDB.py
@@ -15,25 +15,80 @@
 # this test PDB has a net charge of +1, but the correct number of counterions consistent with
 # the salt concentration are automatically added to form a neutral system
 #
-#
 # Vincent Voelz
 # May 21, 2007
 #
+
+# import mmtools.gromacstools.system as system     # the old gromacstools way
 from mmtools.gromacstools.System import *
-import os, sys
 
-# SET UP A GROMACS SIMULATION
-pdbfile = 'test.pdb'            # PDB file containing protein to solvate
-forcefield = 'ffamber99p'       # gromacs forcefield name to use for grompp
-salt = 'NaCl'			# salt pair for counterions - supported types are in the <forcefield>.rtp file
-saltconc = 0.150		# salt concentration in solution (Molarity) # JDC: Should use Units class for this after modifying ioncalc.py to use Units too.
+import os
+import os.path
+import sys
+#=============================================================================================
 
-g = System(pdbfile, useff=forcefield)
-# g.setup.setSaltConditions('NaCl', 0.150 )
-# JDC: Presumably you meant to use the variables you defined up there.
-g.setup.setSaltConditions(salt, saltconc)
+# DEBUG flag: if true then save outputs
+DEBUG = True
 
-# prepare a system, writing TPR and GRO files, and *mdrun* script in the current directory
-thisdir = os.path.abspath( os.curdir )
-g.prepare(outname='prod',outdir=thisdir)
+
+#----------------------------------
+# CLASSES
+#----------------------------------
+
+class PipelineProtein:
+
+    def __init__(self, pdb=None, seq=None, salt='NaCl', saltconc=0.100, pH=7.0):
+        """Initialize a default data in protein object."""
+        
+        self.pdbfile  = pdb
+        self.seqfile  = seq
+        self.salt     = salt         # Molar concentration of salt (ionic strength)
+        self.saltconc = saltconc
+        self.pH       = pH
+
+    
+#----------------------------------
+# FUNCTIONS
+#----------------------------------
+        
+ 
+def shoveItThrough(protein, forcefield):
+    
+    thisdir = os.curdir
+    
+    # determine base name
+    print "PDB File: " + protein.pdbfile
+    print "Sequence File: " + protein.seqfile
+    extInd = protein.seqfile.rindex(".")
+    baseName = protein.seqfile[0:extInd]
+        
+    # run gromacs setup
+    if (1):
+        outname = baseName + "_final"
+        # g = system.GromacsSystem(mcceOut, useff=forcefield)    # the old gromacstools way
+        g = System(protein.pdbfile, useff=forcefield)
+        g.setup.setSaltConditions(protein.salt, protein.saltconc)
+        g.setup.set_boxSoluteDistance(0.9)   # <--- ***Greg!!!*** <--- periodic box margin distance, in nanometers 
+        outdir = os.path.join(thisdir, baseName)
+        print 'Writing equilibration directory to',outdir,'...'
+        if os.path.exists(outdir) == False:
+            os.mkdir(outdir)
+        g.prepare(outname=outname, outdir=outdir, verbose=True, cleanup=False, debug=DEBUG, protocol='racecar2', checkForFatalErrors=True)
+    
+
+#----------------------------------
+# MAIN
+#----------------------------------
+
+if __name__ == '__main__':
+
+    pdbfile = 'test.pdb'            # PDB file containing protein to solvate
+    sequence = 'test.seq'           # file or string containing the one-letter sequence
+    forcefield = 'ffamber99p'       # gromacs forcefield name to use for grompp
+    salt = 'NaCl'	            # salt pair for counterions - supported types are in the <forcefield>.rtp file
+    saltconc = 0.150	
+    pH = 7.0
+        
+    shoveItThrough( PipelineProtein( pdb=pdbfile, seq=sequence, salt=salt, saltconc=saltconc, pH=pH), forcefield=forcefield  )
+        
 
