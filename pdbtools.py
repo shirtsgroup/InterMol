43ce94fdc9a30a718217b5f8678749a160b75745
vvoelz <vvoelz@a9e5ec5c-6028-0410-9666-b03508a98303>
2007-08-21 19:02:11 +0000

    Added termini conversion tools.

--- pdbtools.py
+++ pdbtools.py
@@ -3,7 +3,9 @@
 # PDB reading and writing functions from packmoltools/resolvate, which provide
 # rudimentry input and output for pdbfiles.
 #
-# written by John Chodera, modified by Vincennt Voelz, Stanford University (c) 2007
+# written by John Chodera
+# modified by Vincent Voelz
+# Stanford University (c) 2007
 # -----------------------------------------------------
 #
 # -----------------------------------------------------
@@ -278,7 +280,93 @@ def writePDBFromGrofile(grofile, pdbfile, stripWaters=False, stripIons=False):
     writeAtomsToPDB(pdbfile, atoms, renumber = True)
         
         
+def rebuildPatchedTermini(outPdbFile):
+    """Takes in a PDB file with 'patched' termini from Modeller, and rewrites it with the ffAMBER-style
+    ACE and NH2 and residues."""
     
-    
+    # The Modeller PDBs look like this:
+    """
+     EXPDTA    THEORETICAL MODEL, MODELLER 9v1 2007/08/20 13:48:30 
+     REMARK   6 MODELLER OBJECTIVE FUNCTION:      4986.1367
+     REMARK   6 MODELLER BEST TEMPLATE % SEQ ID: 100.000
+     ATOM      1  N   LEU     1      -7.461   4.812   8.221  1.00387.74       1SG   2
+     ATOM      2  CA  LEU     1      -7.033   6.174   8.059  1.00387.74       1SG   3
+     ATOM      3  CB  LEU     1      -7.447   7.022   9.265  1.00387.74       1SG   4
+     ATOM      4  CG  LEU     1      -8.557   8.090   8.962  1.00387.74       1SG   5
+     ATOM      5  CD1 LEU     1      -8.754   8.471   7.469  1.00387.74       1SG   6
+     ATOM      6  CD2 LEU     1      -9.863   7.852   9.710  1.00387.74       1SG   7
+     ATOM      7  C   LEU     1      -5.625   6.415   7.559  1.00387.74       1SG   8
+     ATOM      8  O   LEU     1      -4.618   5.810   7.920  1.00387.74       1SG   9
+cap* ATOM      9  CAY LEU     1      -9.681   4.051   7.379  1.00387.74       1SG  10
+cap* ATOM     10  CY  LEU     1      -8.330   4.670   7.236  1.00387.74       1SG  11
+cap* ATOM     11  OY  LEU     1      -8.069   5.220   6.173  1.00387.74       1SG  12
+     ATOM     12  N   SER     2      -5.488   7.387   6.667  1.00239.19       1SG  13
+     ATOM     13  CA  SER     2      -5.907   8.699   7.035  1.00239.19       1SG  14
+     ....
+     ATOM    279  C   LEU    34     -14.712  13.829   3.507  1.00350.49       1SG 280
+     ATOM    280  O   LEU    34     -13.790  13.944   2.701  1.00350.49       1SG 281
+     ATOM    281  N   PHE    35     -14.937  14.531   4.654  1.00213.39       1SG 282
+     ATOM    282  CA  PHE    35     -14.196  14.588   5.890  1.00213.39       1SG 283
+     ATOM    283  CB  PHE    35     -14.683  14.059   7.205  0.50213.39       1SG 284
+     ATOM    284  CG  PHE    35     -15.624  14.612   8.170  0.50213.39       1SG 285
+     ATOM    285  CD1 PHE    35     -15.385  15.592   9.112  0.50213.39       1SG 286
+     ATOM    286  CD2 PHE    35     -16.780  13.907   8.118  0.50213.39       1SG 287
+     ATOM    287  CE1 PHE    35     -16.409  15.856   9.992  0.50213.39       1SG 288
+     ATOM    288  CE2 PHE    35     -17.793  14.160   8.976  0.50213.39       1SG 289
+     ATOM    289  CZ  PHE    35     -17.589  15.139   9.915  0.50213.39       1SG 290
+     ATOM    290  C   PHE    35     -13.342  13.381   5.900  1.00213.39       1SG 291
+     ATOM    291  O   PHE    35     -12.488  13.238   5.039  1.00213.39       1SG 292
+*cap ATOM    292  NT  PHE    35     -13.553  12.516   6.942  1.00213.39       1SG 293
+     END
 
+"""
+
+            
+    # Copy the original PDB to a temp file, to preserve a record off it
+    tmpPdbFile = outPdbFile + '.before'
+    fin = open(outPdbFile,'r')
+    fout = open(tmpPdbFile, 'w')
+    fout.write( fin.read() )
+    fin.close()
+    fout.close()
+    
+    # Read in the atoms from the PDB
+    atoms = readAtomsFromPDB( outPdbFile )
+    
+    # Find the atoms in the termini caps
+    nter_atoms = []
+    cter_atoms = []
+    i=0
+    while i < len(atoms):
+        if atoms[i]['name'].strip() == 'CY':
+            atoms[i]['name'] = ' C  '
+            nter_atoms.insert(0, atoms.pop(i))
+        elif atoms[i]['name'].strip() == 'CAY':
+            atoms[i]['name'] = ' CA '
+            nter_atoms.insert(0, atoms.pop(i) )
+        elif atoms[i]['name'].strip() == 'OY':
+            atoms[i]['name'] = ' O  '
+            nter_atoms.insert(0, atoms.pop(i) )
+        elif atoms[i]['name'].strip() == 'NT':
+            atoms[i]['name'] = ' N  '
+            cter_atoms.insert(0, atoms.pop(i) )                
+        else:
+            i += 1 
+            
+    # print 'nter_atoms', nter_atoms, 'cter_atoms', cter_atoms
+    
+    # Rename, shift numbering, and tack back onto the atoms list
+    for atom in nter_atoms:
+        atom['resName'] = 'ACE '
+        atom['resSeq'] = atom['resSeq'] - 1
+        atoms.insert(0, atom)
+    for atom in cter_atoms:
+        atom['resName'] = 'NH2 '
+        atom['resSeq'] = atom['resSeq'] + 1
+        atoms.append( atom )
+    
+    # write he atoms to PDB -- this will take care of renumbering too
+    writeAtomsToPDB(outPdbFile, atoms, renumber = True)
+    
+    
     
