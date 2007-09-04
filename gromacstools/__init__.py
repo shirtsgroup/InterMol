f48ae67e8420cc96a6c044e22051a3a5875b78ad
vvoelz <vvoelz@a9e5ec5c-6028-0410-9666-b03508a98303>
2007-09-04 05:33:35 +0000

    Fixed/Added to __init__.py

--- gromacstools/__init__.py
+++ gromacstools/__init__.py
@@ -1,2 +1,4 @@
-__all__ = ["System","Filenames","Setup","Status","IonCalculator"]
+__all__ = ["Filenames", "GromacsData", "IonCalculator", "MdpFile", "Setup", "Status", "System", "TableMaker"]
 
+### global module Exceptions ###
+class McceError(Exception): pass
