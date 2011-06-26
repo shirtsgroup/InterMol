5f11d7bf5fbabfbc87ff60c7255b3e80d484d8b3
ctk3b <ctk3b@a9e5ec5c-6028-0410-9666-b03508a98303>
2011-06-26 15:59:23 +0000

    -n updated test2PPN

--- testfiles/test2PPN.py
+++ testfiles/test2PPN.py
@@ -2,7 +2,7 @@ import ctools.Driver as Driver
 import pdb
 
 Driver.initSystem("Solvated 2PPN")
+Driver.loadTopology("2PPN.top")
 Driver.loadStructure("2PPN.gro")
-Driver.loadctools("2PPN.top")
 Driver.writeStructure("2PPNout.gro")
-Driver.writectools("2PPNout.top")
+Driver.writeTopology("2PPNout.top")
