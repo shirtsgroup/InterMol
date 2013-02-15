b08da7a62e99d2fe9cdff6355a071dcfb30dc66c
hd2zm <hd2zm@a9e5ec5c-6028-0410-9666-b03508a98303>
2013-02-15 03:05:00 +0000

    -n testing other cms examples

--- testfiles/testDesmond/test_Desmond_to_Desmond.py
+++ testfiles/testDesmond/test_Desmond_to_Desmond.py
@@ -5,15 +5,27 @@ System._sys = System("Redone Sample")
 print "System initialized\n"
 
 #READING IN DESMOND--WRITING OUT IN DESMOND
-filename = "Redone.cms"
+#filename = "Redone.cms"
 #filename = "3.cms"
+#filename = "a2a_dppc-out.cms"
+#filename = "desmond_md_job-in.cms"
+#filename = "example.cms"
+#filename = "lck_Me_Cl_complex_12_1-out.cms"
+#filename = "lck_Me_Cl_solvent_12_1-out.cms"
+filename = "simulated_annealing_example.cms"
 print "Reading in Desmond structure %s"%(filename)
 import ctools.DesmondExt.DesmondParser as DesmondParser
 DesmondParser = DesmondParser()
 DesmondParser.readFile(filename)
 
-filename = "Redone_out.cms"
-#filename = "3_out.cms"
+#filename = "Redone_OUT.cms"
+#filename = "3_OUT.cms"
+#filename = "a2a_dppc-out_OUT.cms"
+#filename = "desmond_md_job-in_OUT.cms"
+#filename = "example_OUT.cms"
+#filename = "lck_Me_Cl_complex_12_1-out_OUT.cms"
+#filename = "lck_Me_Cl_solvent_12_1-out_OUT.cms"
+filename = "simulated_annealing_example_OUT.cms"
 print "Writing out Desmond structure %s"%(filename)
 DesmondParser.writeFile(filename)
 
