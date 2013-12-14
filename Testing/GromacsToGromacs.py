c65be131c69f010d9f56ead95f4caa95b7eb36b7
Michael Shirts <michael.shirts@virginia.edu>
2013-12-14 14:41:03 -0500

    Added conversion files back in.

---
+++ Testing/GromacsToGromacs.py
@@ -0,0 +1,28 @@
+import sys
+import os.path
+from ctools.System import System
+
+System._sys = System("Redone Sample")
+print "System initialized\n"
+print "ENTER FILENAME?\n"
+filename = raw_input("")
+while not (os.path.isfile(filename+".gro") or os.path.isfile(filename+".top")):
+ print "File doesn't exit, try again\n"
+ filename = raw_input("")
+filename_out = filename + "_OUT"
+from ctools.GromacsExt.GromacsTopologyParser import GromacsTopologyParser
+print 'Reading in Gromacs topology "%s"...' %(filename)
+if not GromacsTopologyParser._GroTopParser:
+  GromacsTopologyParser._GroTopParser = GromacsTopologyParser()
+GromacsTopologyParser._GroTopParser.parseTopology(filename + '.top')
+import ctools.GromacsExt.GromacsStructureParser as GromacsStructureParser
+print 'Reading in Gromacs structure "%s"...' %(filename)
+GromacsStructureParser.readStructure(filename + '.gro')
+print "\nWriting out Gromacs topology %s"%(filename_out+".top")
+import ctools.GromacsExt.GromacsTopologyParser as GromacsTopologyParser
+GromacsTopologyParser = GromacsTopologyParser()
+GromacsTopologyParser.writeTopology(filename_out+".top")
+print "\nWriting in Gromacs structure %s"%(filename_out+".gro")
+import ctools.GromacsExt.GromacsStructureParser as GromacsStructureParser
+GromacsStructureParser.writeStructure(filename_out+".gro")
+
