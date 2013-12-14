bdc463b1b0ced4be8680e1e83f7a5078e02ca4b9
Michael Shirts <michael.shirts@virginia.edu>
2013-12-14 14:36:49 -0500

    Revising to reorder the files in the way we want and remove unnecessarily big files.

--- Testing/Inputs/DesmondInputs/test.py
+++
@@ -1,4 +0,0 @@
-import sys
-import os.path
-print os.path
-print sys
--- testfiles/testDesmond/desmond_md_job-in/checkFile/checkfile.sh
+++
@@ -1,2 +0,0 @@
-export SCHRODINGER=/opt/schrodinger2011/
-$SCHRODINGER/desmond -P 1 -in desmond_md_job-in.cms -c onepoint.cfg -exec mdsim
