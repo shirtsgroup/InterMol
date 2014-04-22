9fa3f4784949a954907bdea10a6beeec475368cf
Ellen Zhong <ellen.zhong@virginia.edu>
2014-04-22 00:13:50 -0400

    removed testfiles directory

--- testfiles/2PPN/test2PPN.py
+++
@@ -1,8 +0,0 @@
-import ctools.Driver as Driver
-import pdb
-
-Driver.initSystem("Solvated 2PPN")
-Driver.loadTopology("2PPN.top")
-Driver.loadStructure("2PPN.gro")
-Driver.writeStructure("2PPNout.gro")
-Driver.writeTopology("2PPNout.top")
--- testfiles/PreProcess.py
+++
@@ -1,7 +0,0 @@
-from GromacsctoolsParser import *
-
-Parser = GromacsctoolsParser()
-
-for line in Parser.preprocess('2PPN.top'):
-    print line
-
