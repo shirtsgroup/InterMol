a1d0c3706a6ce72dd04fea14607578e6043a8bd8
Jeff Thompson <jpthompson17@gmail.com>
2015-01-15 19:58:27 -0600

    Change 'type' attribute to 'pairtype' in abstract_pair.py.

--- intermol/forces/abstract_pair.py
+++ intermol/forces/abstract_pair.py
@@ -1,10 +1,10 @@
 class AbstractPair(object):
-    __slots__ = ['atom1', 'atom2', 'type']
+    __slots__ = ['atom1', 'atom2', 'pairtype']
 
-    def __init__(self, atom1, atom2, type = None):
+    def __init__(self, atom1, atom2, pairtype = None):
         self.atom1 = atom1
         self.atom2 = atom2
-        self.type = type
+        self.pairtype = pairtype
 
     def __eq__(self, object):
         if ((self.atom1 == object.atom1 and self.object2 == object.atom2) or (self.atom1 == object.atom2 and self.atom2 == object.atom1)):
@@ -13,5 +13,5 @@ class AbstractPair(object):
             return False
 
     def __hash__(self):
-        return hash(tuple([self.atom1, self.atom2]))
+        return hash(tuple(sorted([self.atom1, self.atom2])))
 
