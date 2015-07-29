451c920df6bd72ff0d9d83704c969147a44c8368
Christoph Klein <christoph.t.klein@me.com>
2015-07-29 10:50:54 -0500

    Merge desmond tests and UBNoHarm

--- intermol/moleculetype.py
+++ intermol/moleculetype.py
@@ -1,4 +1,4 @@
-from intermol.orderedset import OrderedSet
+from orderedset import OrderedSet
 
 
 class MoleculeType(object):
@@ -28,6 +28,48 @@ class MoleculeType(object):
     def add_molecule(self, molecule):
         """Add a molecule into the moleculetype. """
         self.molecules.add(molecule)
+
+    # These three functions could probably be made faster through some sort of list comprehension
+    # rather than iterating explicitly over the lists
+
+    def _match_two_atoms(self, newforce, oldforces):
+        """ find and return any force with the same three atoms. For now, respect ordering in matches. """
+        newatoms = [newforce.atom1, newforce.atom2]
+        for force in oldforces:
+            oldatoms = [force.atom1, force.atom2]
+            if newatoms == oldatoms:
+                return force
+        return False
+
+    def _match_three_atoms(self, newforce, oldforces):
+        """ find and return any force with the same three atoms. For now, respect ordering in matches. """
+        newatoms = [newforce.atom1, newforce.atom2, newforce.atom3]
+        for force in oldforces:
+            oldatoms = [force.atom1, force.atom2, force.atom3]
+            if newatoms == oldatoms:
+                return force
+        return False
+
+    def _match_four_atoms(self, newforce, oldforces):
+        """ find and return any force with the same three atoms. For now, respect ordering in matches. """
+        newatoms = [newforce.atom1, newforce.atom2, newforce.atom3, newforce.atom4]
+        for force in oldforces:
+            oldatoms = [force.atom1, force.atom2, force.atom3, force.atom4]
+            if newatoms == oldatoms:
+                return force
+        return False
+
+    def match_bonds(self, bond):
+        self._match_two_atoms(bond, self.bond_forces)
+
+    def match_pairs(self, pair):
+        return self._match_two_atoms(pair, self.pair_forces)
+
+    def match_angles(self, angle):
+        return self._match_three_atoms(angle, self.angle_forces)
+
+    def match_dihedrals(self, dihedral):
+        return self._match_four_atoms(dihedral, self.dihedral_forces)
 
     def __repr__(self):
         return "MoleculeType '{}' with {} molecules".format(
