7ef801c221b186f0cdda6b85e51e01452eb20e03
Christoph Klein <christoph.t.klein@me.com>
2015-08-05 10:51:06 -0500

    Turn off stress tests on Travis

--- intermol/tests/test_all.py
+++ intermol/tests/test_all.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import pytest
 import sys
 
 from intermol.tests.testing_tools import convert_one_to_all, ENGINES
@@ -21,6 +23,7 @@ def test_gromacs_unit(energy=False):
     convert_one_to_all(input_engine='gromacs', test_type='unit', energy=energy)
 
 
+@pytest.mark.skipif(bool(os.getenv("CI")), reason="Running on CI")
 def test_gromacs_stress(energy=False):
     convert_one_to_all(input_engine='gromacs', test_type='stress', energy=energy)
 
@@ -28,7 +31,7 @@ def test_gromacs_stress(energy=False):
 def test_lammps_unit(energy=False):
     convert_one_to_all(input_engine='lammps', test_type='unit', energy=energy)
 
-
+@pytest.mark.skipif(bool(os.getenv("CI")), reason="Running on CI")
 def test_lammps_stress(energy=False):
     convert_one_to_all(input_engine='lammps', test_type='stress', energy=energy)
 
@@ -36,7 +39,7 @@ def test_lammps_stress(energy=False):
 def test_desmond_unit(energy=False):
     convert_one_to_all(input_engine='desmond', test_type='unit', energy=energy)
 
-
+@pytest.mark.skipif(bool(os.getenv("CI")), reason="Running on CI")
 def test_desmond_stress(energy=False):
     convert_one_to_all(input_engine='desmond', test_type='stress', energy=energy)
 
