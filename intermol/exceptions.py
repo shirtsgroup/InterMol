840accf21a22fc6e3bfaceecb5ff92e3269e8292
Christoph Klein <christoph.t.klein@me.com>
2015-08-07 11:58:44 -0500

    Exception doc strings.

--- intermol/exceptions.py
+++ intermol/exceptions.py
@@ -17,28 +17,28 @@ class ConversionError(InterMolError):
 
 
 class UnsupportedFunctional(ConversionError):
-    """"""
+    """Force functional that is not supported in a specific engine."""
     def __str__(self):
         return "{}'s are not supported in {}.".format(
             self.could_not_convert.__class__.__name__, self.engine)
 
 
 class UnimplementedFunctional(ConversionError):
-    """"""
+    """Force functional that is not yet implemented in intermol for a specific engine."""
     def __str__(self):
         return ('{} conversion has not yet been implemented in InterMol '
                 'for {}.'.format(self.could_not_convert.__class__.__name__, self.engine))
 
 
 class UnsupportedSetting(ConversionError):
-    """"""
+    """Any setting that is not supported in a specific engine."""
     def __str__(self):
         return "{} is not supported in {}.".format(
             self.could_not_convert, self.engine)
 
 
 class UnimplementedSetting(ConversionError):
-    """"""
+    """Any setting that is not yet implemented in intermol for a specific engine."""
     def __str__(self):
         return ('{} has not yet been implemented in InterMol for {}.'.format(
             self.could_not_convert, self.engine))
