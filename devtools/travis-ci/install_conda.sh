1667be2177c3feb3bd9e0ac91e877c625cfe8add
Matt Thompson <mattwthomp@gmail.com>
2020-09-29 16:26:17 -0500

    Set up GitHub Actions CI (#367)
    * MAINT: Bump Python versions in CI
    
    * Set up GitHub Actions CI
    
    * Fix test env name
    
    * Drop coverage in tests and Python 3.8
    
    * Add travis back, update miniconda URL
    
    * curl -> wget
    
    * Fix URL
    
    * Fix env name
    
    * Fix typo
    
    * Fix typo
    
    * Update script to cookiecutter
    
    * Fix brew

--- devtools/travis-ci/install_conda.sh
+++ devtools/travis-ci/install_conda.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]];   then MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh; fi
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then MINICONDA=Miniconda3-latest-Linux-x86_64.sh;  fi
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    # Make OSX md5 mimic md5sum from linux, alias does not work
+    md5sum () {
+        command md5 -r "$@"
+    }
+    MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh
+else
+    MINICONDA=Miniconda3-latest-Linux-x86_64.sh
+fi
 
-MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget https://repo.continuum.io/miniconda/$MINICONDA
+MINICONDA_MD5=$(wget -qO- https://repo.anaconda.com/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+wget -q https://repo.anaconda.com/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
