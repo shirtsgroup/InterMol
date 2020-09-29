e790b9e09ebecfd8e5d67bb7f15e73b320f88746
Matthew W. Thompson <mattwthompson@protonmail.com>
2020-09-29 15:45:55 -0500

    Update script to cookiecutter

--- devtools/travis-ci/install_conda.sh
+++ devtools/travis-ci/install_conda.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]];   then MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh; fi
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then MINICONDA=Miniconda3-latest-Linux-x86_64.sh;  fi
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    # Make OSX md5 mimic md5sum from linux, alias does not work
+    md5sum () {
+        command md5 -r "$@"
+    }
+    MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh
+else
+    MINICONDA=Miniconda3-latest-Linux-x86_64.sh
+fi
 
 MINICONDA_MD5=$(wget -qO- https://repo.anaconda.com/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget -q https://repo.anaconda.com/miniconda/$MINICONDA
