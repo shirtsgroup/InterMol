73f5b48157c3f9ee356de4a7d837052d5e8308db
Matthew W. Thompson <mattwthompson@protonmail.com>
2020-09-29 15:08:58 -0500

    curl -> wget

--- devtools/travis-ci/install_conda.sh
+++ devtools/travis-ci/install_conda.sh
@@ -3,7 +3,7 @@
 if [[ "$TRAVIS_OS_NAME" == "osx" ]];   then MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh; fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then MINICONDA=Miniconda3-latest-Linux-x86_64.sh;  fi
 
-MINICONDA_MD5=$(curl -qO- https://repo.continuum.com/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+MINICONDA_MD5=$(wget -qO- https://repo.continuum.com/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget -q https://repo.continuum.com/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
